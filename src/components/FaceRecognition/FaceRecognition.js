d9fe1016418858dad42e383900d131271617d425
Rohan Batra <rohanbatra24@gmail.com>
2020-06-06 13:12:40 -0400

    add inline style to bounding box div

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 const FaceRecognition = (props) => {
 	return (
 		<div className="center ma">
 			<div className="absolute mt2">
 				<img id="inputImage" src={props.imageUrl} alt="" width="500px" height="auto" />
+				<div
+					className="bounding-box"
+					style={{
+						top    : props.box.topRow,
+						right  : props.box.rightCol,
+						bottom : props.box.bottomRow,
+						left   : props.box.leftCol
+					}}
+				/>
 			</div>
 		</div>
 	);
