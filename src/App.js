bf548ebde23f7dca6ec3c5e33807db5999f9e8b2
Rohan Batra <rohanbatra24@gmail.com>
2020-06-06 10:44:57 -0400

    change app component to class based, add onclick,onchange

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
-function App() {
-	return (
-		<div className="App">
-			<Navigation />
-			<Logo />
-			<ImageLinkForm />
-			{/* <FaceRecognition /> */}
-		</div>
-	);
+const particlesOptions = {
+	particles : {
+		line_linked : {
+			shadow : {
+				enable : true,
+				color  : '#3CA9D1',
+				blur   : 5
+			}
+		}
+	}
+};
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input : ''
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = (event) => {
+		console.log('click');
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles" params={particlesOptions} />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+				{/* <FaceRecognition /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
