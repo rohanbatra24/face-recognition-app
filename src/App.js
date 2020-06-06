262e8ccd13d78490765cbda64570575dc6595a63
Rohan Batra <rohanbatra24@gmail.com>
2020-06-06 02:36:47 -0400

    add image link form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
 	return (
 		<div className="App">
 			<Navigation />
 			<Logo />
-			{/* <ImageLinkForm />
-			<FaceRecognition /> */}
+			<ImageLinkForm />
+			{/* <FaceRecognition /> */}
 		</div>
 	);
 }
