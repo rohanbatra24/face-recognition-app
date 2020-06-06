ae905c046534a1d1bfaffa4fd3235571dcf09dca
Rohan Batra <rohanbatra24@gmail.com>
2020-06-06 12:50:19 -0400

    add id attr to image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = (props) => {
 	return (
-		<div className="center">
-			<img src={props.imageUrl} alt="" />
+		<div className="center ma">
+			<div className="absolute mt2">
+				<img id="inputImage" src={props.imageUrl} alt="" width="500px" height="auto" />
+			</div>
 		</div>
 	);
 };
