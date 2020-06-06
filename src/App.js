1fb0c1c9469ef1eeebc69aa5808347ecfafdb891
Rohan Batra <rohanbatra24@gmail.com>
2020-06-06 11:23:49 -0400

    add clarifai post request to on submit handler

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey : 'f84bba4ed61d47ce8ff66f3e22bf02b5'
+});
 
 const particlesOptions = {
 	particles : {
@@ -22,16 +29,28 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			input : ''
+			input    : '',
+			imageUrl : ''
 		};
 	}
 
 	onInputChange = (event) => {
+		this.setState({ input: event.target.value });
 		console.log(event.target.value);
 	};
 
 	onButtonSubmit = (event) => {
+		this.setState({ imageUrl: this.state.input });
 		console.log('click');
+		app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+			function(response) {
+				// do something with response
+				console.log(response);
+			},
+			function(err) {
+				// there was an error
+			}
+		);
 	};
 
 	render() {
@@ -42,7 +61,7 @@ class App extends Component {
 				<Logo />
 				<Rank />
 				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-				{/* <FaceRecognition /> */}
+				<FaceRecognition imageUrl={this.state.imageUrl} />
 			</div>
 		);
 	}
