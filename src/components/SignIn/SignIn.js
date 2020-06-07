8818c421330ab2958301b166959beebd8c120117
Rohan Batra <rohanbatra24@gmail.com>
2020-06-07 11:35:04 -0400

    add onclikc handler to sign in button

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const SignIn = () => {
+export const SignIn = (props) => {
 	return (
 		<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 			<main className="pa4 black-80">
@@ -32,6 +32,7 @@ export const SignIn = () => {
 					</fieldset>
 					<div className="">
 						<input
+							onClick={() => props.onRouteChange('home')}
 							className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
 							type="submit"
 							value="Sign in"
