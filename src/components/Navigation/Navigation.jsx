f0badc0a5a271522dc5a6960d5712872665226fc
Rohan Batra <rohanbatra24@gmail.com>
2020-06-07 11:34:46 -0400

    add onlick handler on sign out button

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = (props) => {
 	return (
 		<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-			<p className="f3 dim link black underline pa3 pointer">Sign out</p>
+			<p onClick={() => props.onRouteChange('signin')} className="f3 dim link black underline pa3 pointer">
+				Sign out
+			</p>
 		</nav>
 	);
 };
