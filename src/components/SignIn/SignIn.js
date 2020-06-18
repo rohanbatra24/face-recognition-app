36e4c932624aed2e282ca9bbe882556cede0a849
Rohan Batra <rohanbatra24@gmail.com>
2020-06-18 12:56:54 -0400

    change for to htmlFor

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,11 +4,11 @@ export const SignIn = (props) => {
 	return (
 		<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 			<main className="pa4 black-80">
-				<form className="measure">
+				<div className="measure">
 					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 						<legend className="f1 fw6 ph0 mh0">Sign In</legend>
 						<div className="mt3">
-							<label className="db fw6 lh-copy f6" for="email-address">
+							<label className="db fw6 lh-copy f6" htmlFor="email-address">
 								Email
 							</label>
 							<input
@@ -19,7 +19,7 @@ export const SignIn = (props) => {
 							/>
 						</div>
 						<div className="mv3">
-							<label className="db fw6 lh-copy f6" for="password">
+							<label className="db fw6 lh-copy f6" htmlFor="password">
 								Password
 							</label>
 							<input
@@ -43,7 +43,7 @@ export const SignIn = (props) => {
 							Register
 						</p>
 					</div>
-				</form>
+				</div>
 			</main>
 		</article>
 	);
