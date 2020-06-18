4c0633371606db2b8def5ddd9e59c60bb66022a1
Rohan Batra <rohanbatra24@gmail.com>
2020-06-18 12:42:21 -0400

    change register link to p tag

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -39,9 +39,9 @@ export const SignIn = (props) => {
 						/>
 					</div>
 					<div className="lh-copy mt3">
-						<a href="#0" className="f6 link dim black db">
+						<p onClick={() => props.onRouteChange('register')} className="f6 link dim black db pointer">
 							Register
-						</a>
+						</p>
 					</div>
 				</form>
 			</main>
