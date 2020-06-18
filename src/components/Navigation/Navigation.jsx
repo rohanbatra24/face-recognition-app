11ccf5434ac051292a611bb0c194530a81a5b2b1
Rohan Batra <rohanbatra24@gmail.com>
2020-06-18 12:41:37 -0400

    add condition to check if signed in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 const Navigation = (props) => {
-	return (
-		<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-			<p onClick={() => props.onRouteChange('signin')} className="f3 dim link black underline pa3 pointer">
-				Sign out
-			</p>
-		</nav>
-	);
+	if (props.isSignedIn) {
+		return (
+			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<p onClick={() => props.onRouteChange('signout')} className="f3 dim link black underline pa3 pointer">
+					Sign out
+				</p>
+			</nav>
+		);
+	}
+	else {
+		return (
+			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<p onClick={() => props.onRouteChange('signin')} className="f3 dim link black underline pa3 pointer">
+					Sign in
+				</p>
+				<p onClick={() => props.onRouteChange('register')} className="f3 dim link black underline pa3 pointer">
+					Register
+				</p>
+			</nav>
+		);
+	}
 };
 
 export default Navigation;
