3ad36832330b74bf8d9786f03faec290b7fdcc09
Rohan Batra <rohanbatra24@gmail.com>
2020-06-22 10:48:22 -0400

    add functionality to update entry count

--- src/App.js
+++ src/App.js
@@ -85,6 +85,19 @@ class App extends Component {
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
 			.then((response) => {
+				if (response) {
+					fetch('http://localhost:3001/image', {
+						method  : 'put',
+						headers : { 'Content-Type': 'application/json' },
+						body    : JSON.stringify({
+							id : this.state.user.id
+						})
+					})
+						.then((response) => response.json())
+						.then((count) => {
+							this.setState(Object.assign(this.state.user, { entries: count }));
+						});
+				}
 				this.displayFaceBox(this.calculateFaceLocation(response));
 			})
 			.catch((err) => console.log(err));
