adf5ffdaf4619284a147b998f53258a942eb91ab
Rohan Batra <rohanbatra@MacBook-Air.local>
2021-02-02 19:15:13 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ class App extends Component {
     if (route === "signout") {
       this.setState({ isSignedIn: false });
     } else if (route === "home") {
-      this.setState({ isSignedIn: true });
+      this.setState({ isSignedIn: false });
     }
 
     this.setState({ route });
