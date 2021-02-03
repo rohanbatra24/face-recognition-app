fb188a389b5ae1c76b5b9efa8aaf8a7844b90a0e
Rohan Batra <rohanbatra@MacBook-Air.local>
2021-02-02 19:11:01 -0500

    update homepage text

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Navigation = (props) => {
-	if (props.isSignedIn) {
-		return (
-			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<p onClick={() => props.onRouteChange('signout')} className="f3 dim link black underline pa3 pointer">
-					Sign out
-				</p>
-			</nav>
-		);
-	}
-	else {
-		return (
-			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<p onClick={() => props.onRouteChange('signin')} className="f3 dim link black underline pa3 pointer">
-					Sign in
-				</p>
-				<p onClick={() => props.onRouteChange('register')} className="f3 dim link black underline pa3 pointer">
-					Register
-				</p>
-			</nav>
-		);
-	}
+  if (props.isSignedIn) {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => props.onRouteChange("signout")}
+          className="f3 dim link black underline pa3 pointer"
+        >
+          Sign out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => props.onRouteChange("home")}
+          className="f3 dim link black underline pa3 pointer"
+        >
+          Home
+        </p>
+        <p
+          onClick={() => props.onRouteChange("signin")}
+          className="f3 dim link black underline pa3 pointer"
+        >
+          Sign in
+        </p>
+        <p
+          onClick={() => props.onRouteChange("register")}
+          className="f3 dim link black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
